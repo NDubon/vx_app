3e20f5f4a8b22dcb358d67c7e122bf60cb770b00
Nicolas Dubon <nicdubon@yahoo.com>
2012-11-22 20:31:38 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VxApp::Application.config.secret_token = '73bdf484c44b65ccd078e1c9b0b9f0dd2e5d69ff5c51fe38739e654ec9e2d28c17f12bef835678f8f71822b7426058b17303c2bb2592190da83f7b6c6b590b92'
